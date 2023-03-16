07e4267866ac3e5f87ac4cc65aeb51f0b1a42b23
Fruity <fruityflunt@gmail.com>
2023-03-16 17:32:43 +0300

    Template FrontEnd

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./index.html";
+import "./index.scss";
+import data from "./modules/test";
+
+const mult = (a, b) => a * b;
+console.log(mult(5, 15));
+console.log(mult(5, 150));
+console.log(data?.console);
